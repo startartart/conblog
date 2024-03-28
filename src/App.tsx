12ab6605d967e20253902e435a82d390fa15dc1c
ParkByunggwon <startartart@naver.com>
2024-03-28 16:55:42 +0900

    chore: 페이지 및 필요 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <>
-      Hello
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/*" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
