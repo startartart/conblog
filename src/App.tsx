8238f13626c4747188ebb7aed09c84ed9c2a1b00
ParkByunggwon <startartart@naver.com>
2024-03-29 16:09:42 +0900

    chore: css reset 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import { Reset } from 'styled-reset';
 
 function App() {
   return (
     <BrowserRouter>
+      <Reset />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/*" element={<Projects />} />
