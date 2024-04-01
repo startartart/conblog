d7e89fb17340ffdbfe810bba89991a13eda58a3b
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-04-01 21:38:37 +0900

    [Style] Page -  Projects 스타일링
    * chore: css reset 적용
    
    * style: Home Link 임시 연결 및 일부 컴포넌트 스타일링
    
    * chore: 반응형 라이브러리 설치
    
    * style: color 변경
    
    * feat: useCurrentMedia hook
    method에 따라 value 설정
    
    * feat: display 반응 최소 width 설정
    
    * style: Projects 페이지 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import { Reset } from 'styled-reset';
+import styled from 'styled-components';
+import useCurrentMedia from './hooks/useCurrentMedia';
 
 function App() {
+  const value = useCurrentMedia({ method: 'display' });
+  const width = typeof value === 'string' ? value : '';
   return (
     <BrowserRouter>
       <Reset />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects/*" element={<Projects />} />
-      </Routes>
+      <Display $width={width}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects/*" element={<Projects />} />
+        </Routes>
+      </Display>
     </BrowserRouter>
   );
 }
+
+const Display = styled.div<{ $width: string }>`
+  width: ${(props) => props.$width};
+  height: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export default App;
---
+++ src/hooks/useCurrentMedia.ts
@@ -0,0 +1,37 @@
+import { useMediaQuery } from 'react-responsive';
+import { MediaProps, MediaReturnType } from './currentMedia.types';
+
+export default function useCurrentMedia({
+  method,
+}: MediaProps): MediaReturnType {
+  const isHugeDesktop = useMediaQuery({ query: '(min-width: 1920px)' });
+  const isMiddleDesktop = useMediaQuery({
+    query: '(min-width:1420px) and (max-width:1919px)',
+  });
+  const isDesktop = useMediaQuery({
+    query: '(min-width:1024px) and (max-width:1419px)',
+  });
+  const isTablet = useMediaQuery({
+    query: '(min-width:768px) and (max-width:1023px)',
+  });
+  const isMobile = useMediaQuery({
+    query: '(max-width:767px)',
+  });
+
+  if (method === 'row') {
+    if (isMobile) return 1;
+    if (isTablet) return 2;
+    if (isDesktop) return 3;
+    if (isMiddleDesktop) return 4;
+    if (isHugeDesktop) return 5;
+  }
+
+  if (method === 'display') {
+    if (isMobile) return '90%';
+    if (isTablet) return '95%';
+    if (isDesktop) return '1024px';
+    if (isMiddleDesktop) return '1420px';
+    if (isHugeDesktop) return '1880px';
+  }
+  return 0;
+}
