48a4f57478056757660756880ca0c8b03b9f9e9a
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-04-04 16:38:00 +0900

    [Feat] 동적 라우팅 구현 및 적용
    * feat: Projects 데이터 및 이동 구현
    
    * feat: 동적 라우팅 적용
    
    * fix: 프로젝트 리스트 타입 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Project from './pages/Project/Project';
 import { Reset } from 'styled-reset';
 import styled from 'styled-components';
 import useCurrentMedia from './hooks/useCurrentMedia';
@@ -14,7 +15,8 @@ function App() {
       <Display $width={width}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects/*" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/*" element={<Project />} />
         </Routes>
       </Display>
     </BrowserRouter>
