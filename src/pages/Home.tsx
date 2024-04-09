8e970b290d632abb50c77ed5b2d17ae90e3c875d
ParkByunggwon <startartart@naver.com>
2024-04-10 01:35:17 +0900

    feat: 영역 감지 및 스크롤 혹은 슬라이드 이동 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Text from '../components/Text/Text';
 import Footer from '../components/Footer/Footer';
@@ -8,39 +8,84 @@ type BarProps = {
   $direction: 'left' | 'right';
 };
 
+type LayoutProps = {
+  $color: string;
+  $current?: boolean;
+};
+
 export default function Home() {
-  // const [folded, setFolded] = useState(false);
+  const [current, setCurrent] = useState('layout1');
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (layouts) => {
+        layouts.forEach((layout) => {
+          if (layout.isIntersecting) {
+            setCurrent(layout.target.id);
+            layout.target.scrollIntoView();
+          }
+        });
+      },
+      { threshold: 0.001 }
+    );
+
+    const targets = document.querySelectorAll('.layout');
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
-    <Layout>
-      <Title>
-        <Text $fontSize="5rem" $color="#222222">
-          Home
-        </Text>
-      </Title>
-      <Box>
-        <StyleLink to="projects">
-          <Bar $direction="left" />
-          <Bar $direction="right" />
-          <Text $fontSize="2.5rem" $color="#222222" $hoverColor="#bfbfbf">
-            Projects
+    <HomeLayout>
+      <Layout
+        id="layout1"
+        className="layout"
+        $color="#ffffff"
+        $current={'layout1' === current}
+      >
+        <Title>
+          <Text $fontSize="5rem" $color="#222222">
+            Home
           </Text>
-        </StyleLink>
-        <StyleLink to="https://github.com/startartart">
-          <Bar $direction="left" />
-          <Bar $direction="right" />
-          <Text $fontSize="2.5rem" $color="#222222" $hoverColor="#bfbfbf">
-            Github
-          </Text>
-        </StyleLink>
-        <StyleLink to="/">
-          <Text $fontSize="2.5rem" $color="#222222" $isInActive={true}>
-            Posts
-          </Text>
-        </StyleLink>
-      </Box>
-      <Footer />
-    </Layout>
+        </Title>
+        <Box>
+          <StyleLink to="/">
+            <Bar $direction="left" />
+            <Bar $direction="right" />
+            <Text $fontSize="2.5rem" $color="#222222" $isInActive={true}>
+              Projects
+            </Text>
+          </StyleLink>
+          <StyleLink to="https://github.com/startartart">
+            <Bar $direction="left" />
+            <Bar $direction="right" />
+            <Text $fontSize="2.5rem" $color="#222222" $hoverColor="#bfbfbf">
+              Github
+            </Text>
+          </StyleLink>
+          <StyleLink to="/">
+            <Bar $direction="left" />
+            <Bar $direction="right" />
+            <Text $fontSize="2.5rem" $color="#222222" $isInActive={true}>
+              Posts
+            </Text>
+          </StyleLink>
+        </Box>
+      </Layout>
+      <Layout
+        id="layout2"
+        className="layout"
+        $color="#f2f2f2"
+        $current={'layout2' === current}
+      >
+        Introduce
+      </Layout>
+
+      <Footer $show={'layout1' === current} />
+    </HomeLayout>
   );
 }
 
@@ -49,19 +94,35 @@ const StyleLink = styled(Link)`
   width: 100%;
   position: relative;
   text-align: center;
-  user-select: none;
+  /* user-select: none; */
 `;
 
-const Layout = styled.div`
+const HomeLayout = styled.div`
+  overflow: hidden;
+`;
+
+const Layout = styled.div<LayoutProps>`
   width: 100%;
   height: 100vh;
   display: flex;
+  transition: all 0.5s ease-out;
   position: relative;
   gap: 1rem;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  overflow-x: hidden;
+  z-index: 1;
+  border-radius: 5%;
+  background-color: ${(props) => props.$color};
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  ${(props) => props.$current && `animation: fadeIn 1s ease-in-out;`}
 `;
 
 const Box = styled.div`
