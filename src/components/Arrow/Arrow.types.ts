52ca3e61044010788cf3559b2ddc41f8ea35ba04
ParkByunggwon <startartart@naver.com>
2024-04-10 01:34:32 +0900

    feat: 반응형 Footer 및 일부 애니메이션 추가

--- src/components/Arrow/Arrow.types.ts
+++ src/components/Arrow/Arrow.types.ts
@@ -1,3 +1,4 @@
 export type SlideArrowProps = {
   fill?: string;
+  $reverse?: boolean;
 };
--- src/components/Arrow/SlideArrow.tsx
+++ src/components/Arrow/SlideArrow.tsx
@@ -1,17 +1,35 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Arrow from '../../assets/arrow.svg?react';
 import { SlideArrowProps } from './Arrow.types';
 
-export default function SlideArrow({ fill = '#bfbfbf' }: SlideArrowProps) {
-  return <StyledArrow fill={fill} />;
+export default function SlideArrow({
+  fill = '#bfbfbf',
+  $reverse = true,
+}: SlideArrowProps) {
+  return (
+    <ArrowBox $reverse={$reverse}>
+      <StyledArrow fill={fill} />
+    </ArrowBox>
+  );
 }
 
-const StyledArrow = styled(Arrow)`
+const bounce = keyframes`
+  0% {transform: translateY(0);}
+  50% {transform: translateY(-10%);}
+  10% {transform: translateY(0);}
+`;
+
+const ArrowBox = styled.div<SlideArrowProps>`
   width: 100%;
   height: 15%;
-  transition: all 1s;
-  transform: translateY(0px);
-  &:hover {
-    transform: translateY(-20px);
-  }
+  transform: ${(props) => (props.$reverse ? 'scaleY(-1)' : 'none')};
+`;
+
+const StyledArrow = styled(Arrow)<SlideArrowProps>`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 999;
+
+  animation: ${bounce} 3s linear infinite;
 `;
