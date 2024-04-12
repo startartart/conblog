ff2e254d16bd444c76b4c86b0f0cc561af9ef416
ParkByunggwon <startartart@naver.com>
2024-04-12 17:43:54 +0900

    feat: Post 구현 및 동적 라우팅 적용

--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from 'react';
 import Markdown from 'react-markdown';
-import mark from '../../../README.md';
+import { useLocation } from 'react-router-dom';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import './github-markdown.css';
 
 export default function Post() {
+  const { state } = useLocation();
+  const [mark, setMark] = useState('');
+
+  useEffect(() => {
+    import(`./markdown/${state[1]}.md`)
+      .then((module) => module.default)
+      .then((markdown) => {
+        setMark(markdown);
+      })
+      .catch((error) => {
+        console.error('Error loading Markdown file:', error);
+      });
+  }, []);
+
   return (
     <Markdown className="markdown-body" rehypePlugins={[rehypeRaw, remarkGfm]}>
       {mark}
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,16 +1,49 @@
 import styled from 'styled-components';
 import Header from '../components/Header/Header';
+import PostContainer from './Post/\bPostContainer';
+import { PostData } from '../data/PostData';
+import { useNavigate } from 'react-router-dom';
+import { countTags } from '../utils/countTags';
 
 export default function Posts() {
+  const navigate = useNavigate();
+  const tags = countTags(PostData);
+
+  const goPostPage = (id: number, fileName: string, title: string) => {
+    navigate(`/post/${title}`, { state: [id, fileName, title] });
+  };
+
   return (
     <Layout>
       <Header title="Posts" />
       <MainLayout>
         {/* 여기는 tagLayout - Mobile은 따로 */}
-        <TagsLayout>TagsLayout</TagsLayout>
+        <TagsLayout>
+          {Object.keys(tags).map((tag) => (
+            <Tag key={tag}>
+              {tag} ({tags[tag]})
+            </Tag>
+          ))}
+        </TagsLayout>
 
         {/* 여기는 PostContentLayout - Post 클릭해서 볼 수 있음. */}
-        <PostContentLayout> PostContentLayout</PostContentLayout>
+        <PostContentLayout>
+          {PostData.map((post) => {
+            return (
+              <PostContainer
+                key={post.id}
+                id={post.id}
+                fileName={post.fileName}
+                title={post.title}
+                author={post.author}
+                date={post.date}
+                tags={post.tags}
+                titleImage={post.titleImage}
+                onClick={goPostPage}
+              />
+            );
+          })}
+        </PostContentLayout>
       </MainLayout>
     </Layout>
   );
@@ -23,16 +56,25 @@ const Layout = styled.div`
 
 const MainLayout = styled.div`
   display: flex;
+  margin: 1rem;
 `;
 
 const TagsLayout = styled.div`
   display: flex;
   flex-direction: column;
   width: 15%;
+  padding: 1rem;
+  gap: 1rem;
 `;
 
 const PostContentLayout = styled.div`
   display: flex;
   flex-direction: column;
   width: 85%;
+  padding: 1rem;
+  gap: 2rem;
+`;
+
+const Tag = styled.div`
+  font-size: 0.75rem;
 `;
